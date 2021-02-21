380f5605152df362eaa8590fbcc02b048c7f3d8f
ANS <36783010+answebdev@users.noreply.github.com>
2021-02-20 17:20:59 -0800

    add useEffect for timer Alert timer logic

--- src/Alert.js
+++ src/Alert.js
@@ -1,7 +1,21 @@
 import React, { useEffect } from 'react';
 
-const Alert = () => {
-  return <h2>Alert Component</h2>;
+// These properties (type, msg) passed in come from the Alert state value in App.js (see state in App.js)
+// (the options for type (alert type) are 'danger' and 'success').
+// 'removeAlert' comes from inside the 'return' in App.js.
+const Alert = ({ type, msg, removeAlert }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      // Call 'removeAlert' inside of the callback function:
+      removeAlert();
+      // Run callback function after 3 seconds.
+    }, 3000);
+    // Clean up function:
+    return () => clearTimeout(timeout);
+
+    // We want this to happen when the component renders, so use an empty dependency: []
+  }, []);
+  return <p className={`alert alert-${type}`}>{msg}</p>;
 };
 
 export default Alert;
--- src/App.js
+++ src/App.js
@@ -12,34 +12,60 @@ function App() {
   const [isEditing, setIsEditing] = useState(false);
   // State for editing the ID:
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Check if value is empty: '!name' -> if empty, display alert.
+    // Check if value is empty: '!name'.
+    // If value is empty, display alert.
     if (!name) {
       // Disaply alert
+      // We can just use this 'setAlert' line, especially, if we only do this once here:
+      // setAlert({ show: true, msg: 'please enter value', type: 'danger' });
+
+      // But since we're going to use this logic elsewhere, we'll just create a function below (see 'showAlert' below),
+      // and then just use it wherever we need it, such as here:
+      showAlert(true, 'danger', 'please enter value');
+
+      // Hide alert after 3 seconds.
+      // This will be done with 'useEffect' in Alert.js.
+      // This is passed in as a prop called 'removeAlert' from down below in the 'return'.
     }
-    // If there IS a value, AND if editing, then...
+    // If there IS a value, AND if user is editing, then edit.
     else if (name && isEditing) {
       // Deal with Edit
     }
-    // Add item to list
+    // If there is a value and user is NOT editing, create a new item
+    // (with the properties of ID and Title), and add it to the list.
     else {
       // Show alert
 
       // Create new item.
       // We need a unique ID for each new item, so we're going to cheat here and use 'Date().getTime()' to do that:
       const newItem = { id: new Date().getTime().toString(), title: name };
+      // Grab the old values from the list ('...list'), and add the new item ('newItem').
       setList([...list, newItem]);
+      // Clear the input field after submitting.
       setName('');
     }
+  };
+
+  // Create a function (show alert if submitted with empty input field) because this logic will be used in more than one place.
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg });
   };
 
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {/* By default, 'show' in 'showAlert' (see above) is false, so since this is passed into 'removeAlert' here, it will be 'false',
+        which means it will not show, which is what we want for 'removeAlert', and which we will use in Alert.js in 'useEffect'
+        to set the timer so that the alert is removed after 3 seconds. */}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>Shopping List</h3>
         <div className='form-control'>
           <input
@@ -54,10 +80,13 @@ function App() {
           </button>
         </div>
       </form>
-      <div className='grocery-container'>
-        <List items={list} />
-        <button className='clear-btn'>clear items</button>
-      </div>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          {/* Pass in the 'list' as a prop into the List component - here, the prop is named 'items' (see List.js, where it's destructured).*/}
+          <List items={list} />
+          <button className='clear-btn'>clear items</button>
+        </div>
+      )}
     </section>
   );
 }
