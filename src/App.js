fb62b8c0e23bbf90c1d5e0628c61ab8528a41eec
ANS <36783010+answebdev@users.noreply.github.com>
2021-02-20 16:06:46 -0800

    set up state

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from 'react';
 import List from './List';
 import Alert from './Alert';
 
+// Source: https://www.youtube.com/watch?v=a_7Z7C_JCyo
+
 function App() {
-  return <h2>Shopping List App</h2>;
+  const [name, setName] = useState('');
+
+  // This will eventually be stored in local storage:
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  // State for editing the ID:
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+
+  return <section className='section-center'>Shopping List App</section>;
 }
 
 export default App;
